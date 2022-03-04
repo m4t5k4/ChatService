0546804a5ba7582829d27b1497b7cc78596c9aa8
Matthias Reynders <matthias.reynders3@gmail.com>
2022-03-04 19:32:36 +0100

    first commit

---
+++ mychat.frontend/src/components/SendMessageForm.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+
+const SendMessageForm = ({ sendMessage }) => {
+    const [message, setMessage] = useState('');
+
+    return <Form onSubmit={e => {
+        e.preventDefault();
+        sendMessage(message);
+        setMessage('');
+    }}>
+        <InputGroup>
+            <FormControl placeholder="message..."
+                onChange={e => setMessage(e.target.value)} value={message} />
+            <InputGroup>
+                <Button variant='primary' type='submit' disabled={!message}>Send</Button>
+            </InputGroup>
+        </InputGroup>
+    </Form>
+}
+
+export default SendMessageForm;
